571fb2845e4c191810ba1f02eeab2445814d7de4
Gabriel Roche <gabirocher@icloud.com>
2024-12-02 12:37:42 +0100

    Commentaires

--- app/Controllers/ControleurBorne.php
+++ app/Controllers/ControleurBorne.php
@@ -32,6 +32,9 @@ class ControleurBorne extends BaseController {
 	/** @var MatiereModel $matiereModel */
 	private MatiereModel $matiereModel;
 	
+	/**
+	 * Constructeur du contrôleur Borne.
+	 */
 	public function __construct() {
 		$this->borneModel = new BorneModel();
 		$this->boutonModel = new BoutonModel();
@@ -45,13 +48,27 @@ class ControleurBorne extends BaseController {
 	/*  Méthodes des routes tout public  */
 	/*===================================*/
 	
-	public function index(string $theme) : string {
+	/**
+	 * Méthode qui affiche la liste des bornes prédéfinies.
+	 *
+	 * @param string|null $theme Le thème des bornes à sélectionner.
+	 * @return string La vue qui liste les bornes prédéfinies.
+	 */
+	public function index(string $theme = null) : string {
+		
+		
 		return view('borne/index_borne', [
 			'titre' =>"Liste des bornes prédéfines",
 			'bornes'=>$this->borneModel->getBorneParTheme($theme),
 		]);
 	}
 	
+	/**
+	 * Affiche une borne prédéfinie en fonction de son identifiant.
+	 *
+	 * @param int $id_borne L'identifiant de la borne à afficher.
+	 * @return string La vue d'une borne.
+	 */
 	public function voirBorne(int $id_borne) : string {
 		return view('borne/voir_borne', [
 			'titre'=>"Voir la borne",
@@ -59,11 +76,16 @@ class ControleurBorne extends BaseController {
 		]);
 	}
 	
-	// TODO: $id_borne = null, si borne perso ?
-	public function editBorne(int $id_borne) : string {
+	/**
+	 * Affiche la page d'édition de borne.
+	 *
+	 * @param int|null $id_borne L'identifiant de la borne à modifier, ou <code>null</code> si borne personnalisée.
+	 * @return string La vue qui affiche la page de création/modification de borne prédéfinie.
+	 */
+	public function editBorne(int $id_borne = null) : string {
 		return view('borne/edit_borne', [
 			'titre'=>"Modifier le borne",
-			'borne'=>$this->borneModel->getBorneParId($id_borne),
+			'borne'=>$id_borne ? $this->borneModel->getBorneParId($id_borne) : null,
 		]);
 	}
 	
