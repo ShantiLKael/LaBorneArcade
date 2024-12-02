c56397c3c68e1f25daa472228e4027d78ebe9f34
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-12-02 09:28:48 +0100

    Config des routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,41 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->group('', ['filter' => 'invite'], function($routes) {
+	// Page vitrine
+	$routes->get('/'                  , 'HomeController::index');
+	$routes->get('/contact'           , 'HomeController::contact');
+	$routes->get('/qui-sommes-nous'   , 'HomeController::quiSommesNous');
+	$routes->get('/faq'               , 'HomeController::faq');
+	$routes->get('/condition-de-vente', 'HomeController::cgv');
+
+	// Connexion
+	$routes->get('/connexion'  , 'LoginController::connexion');
+	$routes->get('/inscription', 'LoginController::inscription');
+	$routes->get('/connexion/oubli-mdp'       , 'LoginController::oubliMdp');
+	$routes->get('/connexion/oubli-mdp/(:any)', 'LoginController::resetMdp');
+
+	// Blog articles
+	$routes->get('/blog-articles'       , 'ArticleBlogController::index');
+	$routes->get('/blog-articles/(:num)', 'ArticleBlogController::index');
+
+	// Bornes
+	$routes->get('/bornes'       , 'BorneController::index');
+	$routes->get('/bornes/(:num)', 'BorneController::voirBorne');
+	$routes->match(['get', 'post'], '/bornes-perso/(:num)', 'BorneController::editBorne');
+
+	// Panier
+	$routes->get('/panier', 'CommandeController::panier');
+//});
+
+//$routes->group('', ['filter' => 'auth'], function($routes) {
+	$routes->get  ('/commandes', 'CommandeController::index');
+	$routes->match(['get', 'post'], '/profile', 'LoginController::profile');
+//});
+
+//$routes->group('', ['filter' => 'admin'], function($routes) {
+	$routes->match(['get', 'post', 'delete'], '/admin/bornes'  , 'AdminController::index');
+	$routes->match(['get', 'post', 'delete'], '/admin/contact' , 'AdminController::adminContact');
+	$routes->match(['get', 'post', 'delete'], '/admin/articles', 'AdminController::adminArticle');
+	$routes->match(['get', 'post', 'delete'], '/admin/faqs'    , 'AdminController::adminFaq');
+//});
