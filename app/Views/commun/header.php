931ed45233df6fe70b92ad8131241d31fdd9860d
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-12-06 00:58:34 +0100

    bouton de compte vue

--- app/Views/commun/header.php
+++ app/Views/commun/header.php
@@ -20,10 +20,18 @@
             <li><a href="/blog-articles"   class="link-underline link-underline-black">Blog</a></li>
             <?php if (session()->has('user')) : ?>
                 <li class="flex items-center">
-                    <a href="/compte" class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full bg-gray-600">
+                    <a href="/compte" type="button" data-dropdown-toggle="userDropdown" class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full bg-gray-600">
                         <span class="font-medium text-gray-300 uppercase"><?= substr(session()->get('user')['email'], 0, 4) ?></span>
                     </a>
                 </li>
+            <?php else : ?>
+            <li class="flex items-center">
+                <a href="/inscription" type="button" data-dropdown-toggle="userDropdown" class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden rounded-full bg-gray-600 pl-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                    </svg>
+                </a>
+            </li>
             <?php endif; ?>
         </ul>
     </nav>
