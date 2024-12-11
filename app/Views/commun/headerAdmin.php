814906e5c6c0a5fbb9dccf93d11d3fa141d8dfab
julia123456789037 <julia037.filia@gmail.com>
2024-12-11 10:48:01 +0100

    css

--- app/Views/commun/headerAdmin.php
+++ app/Views/commun/headerAdmin.php
@@ -9,23 +9,19 @@
 <header class="flex">
     <!-- Navbar -->
     <nav id="navbar" class="bg-black text-white fixed top-0 left-0 h-screen w-[250px] z-50 flex flex-col px-6 py-4 shadow-lg shadow-gray-700/20 transition-all">
-        <!-- Logo -->
-        <div class="flex items-center mb-6">
-            <img loading="lazy" src="chemin-vers-logo.png" alt="Logo La Borne Arcade" class="h-10 ">
-        </div>
-        <h1 class="font-bold">Administrateur</h1><br>
+        <br><h1 class="font-bold">Administrateur</h1>
         <!-- Liens -->
         <ul id="nav-links" class="flex flex-col space-y-4 text-white font-bold text-lg">
-            <li><a href="/admin/bornes" class="px-4 py-2 rounded hover:bg-gray-700 transition">Ajouter une borne</a></li>
-            <li><a href="/admin/theme" class="px-4 py-2 rounded hover:bg-gray-700 transition">Thème</a></li>
-            <li><a href="/admin/matiere" class="px-4 py-2 rounded hover:bg-gray-700 transition">Matière</a></li>
-            <li><a href="/admin/option" class="px-4 py-2 rounded hover:bg-gray-700 transition">Option</a></li>
-            <li><a href="/admin/joystick" class="px-4 py-2 rounded hover:bg-gray-700 transition">Joystick</a></li>
-            <li><a href="/admin/TMolding" class="px-4 py-2 rounded hover:bg-gray-700 transition">TMolding</a></li>
-            <li><a href="/admin/bouton" class="px-4 py-2 rounded hover:bg-gray-700 transition">Bouton</a></li>
-            <li><a href="/admin/contact" class="px-4 py-2 rounded hover:bg-gray-700 transition">Contact</a></li>
-            <li><a href="/admin/articles" class="px-4 py-2 rounded hover:bg-gray-700 transition">Articles</a></li>
-            <li><a href="/admin/faqs" class="px-4 py-2 rounded hover:bg-gray-700 transition">FAQs</a></li>
+            <li class="m-5"><a href="/admin/bornes" class="px-4 py-2 rounded hover:bg-gray-700 transition">Ajouter une borne</a></li>
+            <li class="m-5"><a href="/admin/theme" class="px-4 py-2 rounded hover:bg-gray-700 transition">Thème</a></li>
+            <li class="m-5"><a href="/admin/matiere" class="px-4 py-2 rounded hover:bg-gray-700 transition">Matière</a></li>
+            <li class="m-5"><a href="/admin/option" class="px-4 py-2 rounded hover:bg-gray-700 transition">Option</a></li>
+            <li class="m-5"><a href="/admin/joystick" class="px-4 py-2 rounded hover:bg-gray-700 transition">Joystick</a></li>
+            <li class="m-5"><a href="/admin/TMolding" class="px-4 py-2 rounded hover:bg-gray-700 transition">TMolding</a></li>
+            <li class="m-5"><a href="/admin/bouton" class="px-4 py-2 rounded hover:bg-gray-700 transition">Bouton</a></li>
+            <li class="m-5"><a href="/admin/contact" class="px-4 py-2 rounded hover:bg-gray-700 transition">Contact</a></li>
+            <li class="m-5"><a href="/admin/articles" class="px-4 py-2 rounded hover:bg-gray-700 transition">Articles</a></li>
+            <li class="m-5"><a href="/admin/faqs" class="px-4 py-2 rounded hover:bg-gray-700 transition">FAQs</a></li>
         </ul>
     </nav>
 
