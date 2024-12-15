99a6386e63a1e0d0fe3591138395c534d6451d50
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-12-16 00:01:20 +0100

    images manquantes en svg

--- app/Views/borne/edit_borne.php
+++ app/Views/borne/edit_borne.php
@@ -52,21 +52,29 @@ view('commun/header', ['titre' => $titre]) ?>
 				</div>
 	
 				<!-- Infos supplémentaires -->
-				<div class="grid grid-cols-2 sm:grid-cols-4 gap-4 p-10 md:p-0 md:mt-5 mt-10">
-					<div class="text-center">
-						<img loading="lazy" src="fabrique_france_icon.png" alt="Fabriqué en France" class="w-10 mx-auto mb-2">
+				<div class="grid grid-cols-2 sm:grid-cols-4 gap-4 mt-6">
+					<div class="flex flex-col border border-gray-700 p-2 rounded-xl items-center justify-center text-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+						</svg>
 						<p class="text-sm">Fabriqué en France</p>
 					</div>
-					<div class="text-center">
-						<img loading="lazy" src="garantie_icon.png" alt="Garantie 2 ans" class="w-10 mx-auto mb-2">
+					<div class="flex flex-col border border-gray-700 p-2 rounded-xl items-center justify-center text-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mb-2">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+						</svg>
 						<p class="text-sm">Garantie 2 ans</p>
 					</div>
-					<div class="text-center">
-						<img loading="lazy" src="support_icon.png" alt="Support" class="w-10 mx-auto mb-2">
+					<div class="flex flex-col border border-gray-700 p-2 rounded-xl items-center justify-center text-center">
+						<svg class="h-8 w-15" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+						</svg>
 						<p class="text-sm">Assistance disponible</p>
 					</div>
-					<div class="text-center">
-						<img loading="lazy" src="paiement_icon.png" alt="Paiement sécurisé" class="w-10 mx-auto mb-2">
+					<div class="flex flex-col border border-gray-700 p-2 rounded-xl items-center justify-center text-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+						</svg>
 						<p class="text-sm">Paiement 100% sécurisé</p>
 					</div>
 				</div>
